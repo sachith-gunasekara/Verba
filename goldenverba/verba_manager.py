d9b2e49d9597d46076bde07a966156a8aa98a501
Sachith Gunasekara <sachith.g@arcadeagroup.com>
2026-01-21 18:57:42 +0530

    refactor: Update rag_config access patterns for improved readability
    - Changed access to the `rag_config` dictionary in `verba_manager.py`, `managers.py` to use dot notation for better clarity and consistency.
    - This refactor enhances code readability without altering functionality.

--- goldenverba/verba_manager.py
+++ goldenverba/verba_manager.py
@@ -716,8 +716,8 @@ class VerbaManager:
         labels: list[str] = [],
         document_uuids: list[str] = [],
     ):
-        retriever = rag_config["Retriever"]["selected"]
-        embedder = rag_config["Embedder"]["selected"]
+        retriever = rag_config["Retriever"].selected
+        embedder = rag_config["Embedder"].selected
 
         await self.weaviate_manager.add_suggestion(client, query)
 
